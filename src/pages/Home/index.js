6276e77a4ea060e3197b7f33e0b6ddd65b7836c8
Raghvenda Kumar Singh <raghvendra14fb@gmail.com>
2022-07-15 18:54:18 +0530

    added navigation bar and Search mentor in homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,10 +3,14 @@ import React from 'react';
 //Components
 import CompanyLogo from '../../components/CompanyLogo';
 import Footer from '../../components/Footer';
+import HomePageNavbar from '../../components/HomePageNavbar';
+import HomePageHeader from '../../components/HomePageHeader';
 
 const Home = () => {
   return (
     <>
+      <HomePageNavbar />
+      <HomePageHeader />
       <CompanyLogo />
       <Footer />
     </>
